ecdb8985ce4b4c57b5e76bfe567084e7d8fea9e0
siparat <akimov1712@yandex.ru>
2025-07-30 15:08:39 +0300

    v0.0.6

--- auth/login.schema.ts
+++ auth/login.schema.ts
@@ -1,6 +1,6 @@
 import z from 'zod'
 
 export const LoginSchema = z.object({
-	username: z.string({ message: 'Не указано имя пользователя' }),
-	password: z.string({ message: 'Не указан пароль' })
+	username: z.string({ message: 'Не указано имя пользователя' }).min(4, { message: 'Минимальная длина имени – 4 символа' }).max(32, { message: 'Максимальная длина имени – 32 символа' }),
+	password: z.string({ message: 'Не указан пароль' }).min(4, { message: 'Минимальная длина пароля – 4 символа' }).max(64, { message: 'Максимальная длина пароля – 64 символа' })
 });
