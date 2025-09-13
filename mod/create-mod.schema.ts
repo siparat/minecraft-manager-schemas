de4a3ff5b27865a01c7eab6bd38575a5ab0f6715
siparat <akimov1712@yandex.ru>
2025-09-13 13:22:03 +0300

    v0.0.19

--- mod/create-mod.schema.ts
+++ mod/create-mod.schema.ts
@@ -5,14 +5,19 @@ export const CreateModSchema = z.object({
 		.string()
 		.min(2, { message: 'Минимальная длина заголовка – 2 символа' })
 		.max(50, { message: 'Максимальная длина заголовка – 50 символов' }),
-	description: z
-		.string()
-		.min(50, { message: 'Минимальная длина описания – 50 символов' })
-		.max(1500, { message: 'Максимальная длина описания – 1500 символов' }),
-	descriptionImages: z.array(z.string().regex(/^\/(?:[^/]+\/)*[^/]+\.webp$/, { message: 'Используйте загруженное изображение' })),
-	image: z.string().regex(/^\/(?:[^/]+\/)*[^/]+\.webp$/, { message: 'Используйте загруженное изображение' }),
+	description: z.string().min(50, { message: 'Минимальная длина описания – 50 символов' }),
+	descriptionImages: z.array(
+		z.string().regex(/^(?:https?:\/\/[^/]+)?(?:\/[^/]+)*\/[^/]+\.(?:png|gif|jpe?g|webp|bmp|svg|tiff)$/, {
+			message: 'Используйте загруженное изображение'
+		})
+	),
+	image: z.string().regex(/^(?:https?:\/\/[^/]+)?(?:\/[^/]+)*\/[^/]+\.(?:png|gif|jpe?g|webp|bmp|svg|tiff)$/, {
+		message: 'Используйте загруженное изображение'
+	}),
 	files: z.array(
-		z.string().regex(/^\/(?:[^/]+\/)*[^/]+\.(zip|mc\w+)$/, { message: 'Используйте загруженный файл мода' })
+		z.string().regex(/^(?:https?:\/\/[^/]+)?(?:\/[^/]+)*\/[^/]+\.(?:zip|mc\w+)$/, {
+			message: 'Используйте загруженный файл мода'
+		})
 	),
 	versions: z.array(
 		z.string().regex(/^\d+\.\d+(\.\d+)?$/, {
