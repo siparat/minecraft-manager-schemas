c193596d16026f575a6b0de8682d917c72c30298
siparat <akimov1712@yandex.ru>
2026-02-01 15:54:54 +0300

    v0.0.36

--- app/recommend-mod.schema.ts
+++ app/recommend-mod.schema.ts
@@ -2,6 +2,5 @@ import z from 'zod';
 
 export const RecommendModSchema = z.object({
 	email: z.string().email({ message: 'Невалидный email' }),
-	description: z.string().max(2024),
-	url: z.string().url({ message: 'Невалидный url' })
+	description: z.string().max(2024)
 })
