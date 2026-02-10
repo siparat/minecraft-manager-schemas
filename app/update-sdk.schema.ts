fbc28d66163fe25707251bca0c3c876f21999b1a
siparat <akimov1712@yandex.ru>
2026-02-10 15:05:02 +0300

    v0.0.41

--- app/update-sdk.schema.ts
+++ app/update-sdk.schema.ts
@@ -8,12 +8,15 @@ export const UpdateSdkSchema = z.object({
 	firstOpenCode: z.string(),
 	firstInterCode: z.string(),
 	firstNativeCode: z.string(),
+	firstBannerCode: z.string(),
 	secondOpenCode: z.string(),
 	secondInterCode: z.string(),
 	secondNativeCode: z.string(),
+	secondBannerCode: z.string(),
 	thirdOpenCode: z.string(),
 	thirdInterCode: z.string(),
 	thirdNativeCode: z.string(),
+	thirdBannerCode: z.string(),
 	delayInter: z.number().min(0),
 	adsNativeType: z.nativeEnum(AdsNativeType, { message: 'Укажите корректный тип нативной рекламы' })
 });
